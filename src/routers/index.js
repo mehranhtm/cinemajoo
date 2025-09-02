87d87cbe3bc241fcc26ec165b0407e3d42097e59
Shirin Mohebi <89771218+Shirin-Mohebi-9943463@users.noreply.github.com>
2025-09-02 10:00:29 +0330

    add a router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,9 @@
-import Signin from '@/views/Signin'
+import Register from '@/views/Register'
+import Login from '@/views/Login'
+import MainPage from '@/views/MainPage'
 
 export default [
-    { path: '/', component: Signin },
+    { path: '/', component: Register },
+    { path: '/login', component: Login },
+    { path: '/mainpage', component: MainPage },
 ]
